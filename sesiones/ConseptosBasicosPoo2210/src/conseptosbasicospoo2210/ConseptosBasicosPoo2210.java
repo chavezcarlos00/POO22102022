349a02e3a3a8cc1605ec8e636d14da978287eb31
chavezcarlos00 <carloschavez316@aragon.unam.mx>
2022-03-02 13:27:26 -0600

    proyecto conceptos basicos

---
+++ sesiones/ConseptosBasicosPoo2210/src/conseptosbasicospoo2210/ConseptosBasicosPoo2210.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package conseptosbasicospoo2210;
+
+/**
+ *
+ * @author Chavez
+ */
+public class ConseptosBasicosPoo2210 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println("hola otra vez");
+    }
+    
+}
