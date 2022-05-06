af5d34c21b36bafda692b6b5fbfd1d766706efcd
chavezcarlos00 <carloschavez316@aragon.unam.mx>
2022-05-06 02:05:57 -0500

    Codigo de Clase

---
+++ tareas/InterfacesUsuario2210/src/ico/fes/iu/EventosRaton.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.fes.iu;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+/**
+ *
+ * @author Chavez
+ */
+public class EventosRaton implements MouseListener{
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("Hola clic de ratón");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        System.out.println("Botón presionado");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        System.out.println("Botón liberado");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        System.out.println("Cursor entró al componente");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        System.out.println("Cursor salió del componente");
+    }
+    
+} 
